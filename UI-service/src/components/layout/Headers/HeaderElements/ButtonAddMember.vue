ef405320342576d418267b2c8049dba2ce709eb7
Vo Nguyen Minh Quan <mquandlvn@gmail.com>
2019-08-23 17:04:20 +0700

    project fix and exception add

---
+++ UI-service/src/components/layout/Headers/HeaderElements/ButtonAddMember.vue
@@ -0,0 +1,21 @@
+<template>
+  <li>
+    <a href="javascript:;" title="Add members to project">
+      <span class="fa fa-child" id="myIcon"></span>
+    </a>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'buttonAddMember'
+}
+</script>
+
+<style scoped>
+#myIcon {
+  color: rgb(43, 42, 42);
+  margin: 0 5px 0 5px
+}
+</style>
+
---
+++ UI-service/src/components/views/ExceptionView/ExceptionItem.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="col-md-3">
+    <div class="box box-success">
+      <div class="box-header with-border">
+        <h3 class="box-title">{{exceptDate.tittle}}</h3>
+    
+        <div class="box-tools pull-left">
+          <button type="button" class="btn btn-box-tool" data-widget="remove" @click="showDialog">
+            <i class="fa fa-times"></i>
+          </button>
+        </div>
+        <!-- /.box-tools -->
+      </div>
+      <!-- /.box-header -->
+      <div class="box-body">02-09-2019 (example)</div>
+      <!-- /.box-body -->
+    </div>
+    <!-- /.box -->
+    <v-dialog/>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'ExceptionItem',
+    props: ['exceptDate'],
+    data() {
+        return {
+        }
+    },
+    methods: {
+    showDialog() {
+      this.$modal.show('dialog', {
+        title: 'Are you sure?',
+        text: 'Do you wish to delete?',
+        buttons: [
+          {
+            title: 'OK',
+            default: true,
+            handler: () => {
+              this.$modal.hide('dialog')
+            }
+          },
+          {
+            title: 'CANCEL',
+            handler: () => {
+              this.$modal.hide('dialog')
+            }
+          }
+        ]
+      })
+    }
+  }
+}
+</script>
