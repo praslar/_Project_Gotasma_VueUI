0d3d2cb2335db4b45ef0d0579fcaa41931769c4a
Vo Nguyen Minh Quan <mquandlvn@gmail.com>
2019-09-09 10:54:35 +0700

    somemore data

--- UI-service/src/components/views/Exception/ManageException.vue
+++ UI-service/src/components/views/Exception/ManageException.vue
@@ -29,17 +29,10 @@
                   name="Tittle"
                   v-validate="'required|min:5'" 
                   v-model="exceptDate.tittle"
-<<<<<<< HEAD
-                  :class="{ 'is-invalid': errors.has('Tittle') }"
-                />
-                <transition name="alert-in" enter-active-class="animated flipInX" leave-active-class="animated flipOutX">
-                  <div class="invalid-feedback" v-if="errors.has('Tittle')">{{ errors.first('Tittle')}}</div>
-=======
                   :class="{ 'is-invalid':  errors.has('Tittle') }"
                 />
                 <transition name="alert-in" enter-active-class="animated flipInX" leave-active-class="animated flipOutX">
                   <div class="invalid-feedback" v-if=" errors.has('Tittle')">{{ errors.first('Tittle')}}</div>
->>>>>>> 636b9742bb487598a648276b988b1c23639128c3
                 </transition>
               </div>
               
@@ -56,15 +49,9 @@
                   width="100%"
                   data-vv-name="Date"
                   v-validate="'required'"
-<<<<<<< HEAD
-                  :class="{ 'is-invalid': errors.has('Date') }"></datepicker>
-                <transition name="alert-in" enter-active-class="animated flipInX" leave-active-class="animated flipOutX">
-                  <div class="invalid-feedback ontop special" v-if="errors.has('Date')">{{ errors.first('Date')}}</div>
-=======
                   :class="{ 'is-invalid':  errors.has('Date') }"></datepicker>
                 <transition name="alert-in" enter-active-class="animated flipInX" leave-active-class="animated flipOutX">
                   <div class="invalid-feedback ontop special" v-if=" errors.has('Date')">{{ errors.first('Date')}}</div>
->>>>>>> 636b9742bb487598a648276b988b1c23639128c3
                 </transition>
               </div>
             </div>
@@ -116,10 +103,7 @@ export default {
   },
   methods: {
     addException() {
-<<<<<<< HEAD
       if (this.exceptDate.date[0] != null && this.exceptDate.date[1] != null) {
-=======
->>>>>>> 636b9742bb487598a648276b988b1c23639128c3
         this.$validator.validateAll().then(result => {
         if (result) {
           this.exceptDate.date[0] = moment(this.exceptDate.date[0]).valueOf()
@@ -127,6 +111,7 @@ export default {
           this.$store.dispatch('addExceptions', this.exceptDate)
         }
       })
+    }
   },
   shortcuts: [
     {
--- UI-service/src/services/projects.js
+++ UI-service/src/services/projects.js
@@ -4,11 +4,10 @@ export const getProjects = async() => {
     return await NetworkHelper.requestGet('/projects')
 }
 
-<<<<<<< HEAD
 export const addProject = async() => {
     return await NetworkHelper.requestGet()
-=======
+}
+
 export const getProjectByID = async(id) => {
     return await NetworkHelper.requestGet('/projects/' + id)
->>>>>>> 636b9742bb487598a648276b988b1c23639128c3
 }
