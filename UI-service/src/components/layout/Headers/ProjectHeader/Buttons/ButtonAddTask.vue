b5a7c6dfadce8781814ed7a473c10c1ee6249fcf
Thang Pham <pxthang97@gmail.com>
2019-09-16 09:35:13 +0700

    add task modal

---
+++ UI-service/src/components/layout/Headers/ProjectHeader/Buttons/ButtonAddTask.vue
@@ -0,0 +1,26 @@
+<template>
+  <li>
+    <a @click="showModal" title="Add Sum Task">
+      <span class="fa fa-file-text-o myIcon"></span>
+    </a>
+  </li>
+</template>
+<script>
+export default {
+  methods: {
+    showModal() {
+        this.$modal.show('add-task')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.myIcon {
+  color: rgb(43, 42, 42);
+  margin: 0 5px 0 5px
+}
+a {
+  cursor: pointer
+}
+</style>
